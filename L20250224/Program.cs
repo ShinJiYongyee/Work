4fa7efb50390c053b2b17a9980958498657d1567
shinjiyong <shinjiyong0904@gmail.com>
2025-02-24 10:15:41 +0900

    비트 컨트롤러

--- L20250224/Program.cs
+++ L20250224/Program.cs
@@ -1,12 +1,35 @@
 ﻿namespace L20250224
 {
+    class BitArray32
+    {
+        public uint Data;   //0000 0000 0000 0000
+
+        public void On(int position)
+        {
+            Data = Data | (uint)(1 << (position - 1));
+        }
+
+        public void Off(int position)
+        {
+            Data = Data & ~(uint)(1 << (position - 1));
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            byte a = 0; //0000 0000
-            a = 1 << 2;
-            Console.WriteLine(a);
+            BitArray32 bitArray32 = new BitArray32();   //0000
+
+            bitArray32.On(3);   //0100
+            bitArray32.On(1);   //0101
+
+            Console.WriteLine(Convert.ToString(bitArray32.Data, 2));
+
+            bitArray32.Off(1);  //0100
+            Console.WriteLine(Convert.ToString(bitArray32.Data, 2));
+
         }
+
     }
+
 }
