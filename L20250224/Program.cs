ed49caab827b10fa39066bcd936bdc76dfe6324e
shinjiyong <shinjiyong0904@gmail.com>
2025-02-24 10:26:13 +0900

    check

--- L20250224/Program.cs
+++ L20250224/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace L20250224
+﻿using System.Formats.Asn1;
+
+namespace L20250224
 {
     class BitArray32
     {
@@ -13,20 +15,29 @@
         {
             Data = Data & ~(uint)(1 << (position - 1));
         }
+
+        public bool Check(uint other)
+        {
+            return (int)(Data & other) > 0 ? true : false;
+        }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            BitArray32 bitArray32 = new BitArray32();   //0000
+            BitArray32 bitArray1 = new BitArray32();   //0000
+            BitArray32 bitArray2 = new BitArray32();
 
-            bitArray32.On(3);   //0100
-            bitArray32.On(1);   //0101
+            bitArray1.On(3);   //0100
+            bitArray1.On(1);   //0101
 
-            Console.WriteLine(Convert.ToString(bitArray32.Data, 2));
+            Console.WriteLine(Convert.ToString(bitArray1.Data, 2));
 
-            bitArray32.Off(1);  //0100
-            Console.WriteLine(Convert.ToString(bitArray32.Data, 2));
+            bitArray1.Off(1);  //0100
+            Console.WriteLine(Convert.ToString(bitArray1.Data, 2));
+
+            Console.WriteLine(bitArray1.Check(bitArray2.Data));
+            
 
         }
 
