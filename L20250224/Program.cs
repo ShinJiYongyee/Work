d919cc5db9d58c85302bdb8ca4de49fe865632cc
shinjiyong <shinjiyong0904@gmail.com>
2025-02-24 09:05:45 +0900

    L20250224

---
+++ L20250224/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace L20250224
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 0; //0000 0000
+            a = 1 << 2;
+            Console.WriteLine(a);
+        }
+    }
+}
