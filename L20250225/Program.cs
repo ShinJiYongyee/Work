c03bb06a9a6e87e9f433ba6c8f95d6b08fac0443
shinjiyong <shinjiyong0904@gmail.com>
2025-02-25 11:57:40 +0900

    dynamicArray2

--- L20250225/Program.cs
+++ L20250225/Program.cs
@@ -1,36 +1,89 @@
-﻿namespace L20250225
+﻿using System.Linq.Expressions;
+
+namespace L20250225
 {
+    class DynamicArray2<T>
+    {
+        protected T[] data;
+        protected int count;
+
+        public DynamicArray2()             //초기화하는 시점 명시
+        {
+            data = new T[10];
+            count = 0;
+        }
+
+        public void Add(T newData)
+        {
+            if (count >= data.Length)
+            {
+                T[] newArray = new T[data.Length * 2];
+                Array.Copy(data, newArray, data.Length);
+                data = newArray;
+            }
+            data[count] = newData;
+            count++;
+
+        }
+
+        public void RemoveAt(int index)
+        {
+            for (int i = index + 1; i < data.Length; i++)
+            {
+                data[i - 1] = data[i];
+            }
+            count--;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return data[index];
+            }
+            set
+            {
+                data[index] = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            DynamicArray dynamicArray = new DynamicArray();
-            dynamicArray.Add(0);
-            Console.WriteLine($"dynamicArray[{0}] = " + dynamicArray[0]);
+            DynamicArray2<int> dynamicArray = new DynamicArray2<int>();
             dynamicArray.Add(1);
-            Console.WriteLine($"dynamicArray[{1}] = " + dynamicArray[1]);
             dynamicArray.Add(2);
-            Console.WriteLine($"dynamicArray[{2}] = " + dynamicArray[2]);
             dynamicArray.Add(3);
-            Console.WriteLine($"dynamicArray[{3}] = " + dynamicArray[3]);
-            
-            Console.WriteLine("\n8번째 인덱스 지움(범위 벗어남)");
-            dynamicArray.RemoveAt(8);
-            for (int i = 0; i < dynamicArray.Count(); i++)
-            {
-                Console.WriteLine($"dynamicArray[{i}] = " + dynamicArray[i]);
-            }
+            dynamicArray.Add(4);
+            dynamicArray.Add(1);
+            dynamicArray.Add(2);
+            dynamicArray.Add(3);
+            dynamicArray.Add(4);
+            dynamicArray.Add(1);
+            dynamicArray.Add(2);
+            dynamicArray.Add(3);
+            dynamicArray.Add(4);
+            dynamicArray.Add(1);
+            dynamicArray.Add(2);
+            dynamicArray.Add(3);
+            dynamicArray.Add(4);
 
-            Console.WriteLine("\n2번째 인덱스 지움");
-            dynamicArray.RemoveAt(2);
-            for (int i = 0; i < dynamicArray.Count(); i++)
-            {
-                Console.WriteLine($"dynamicArray[{i}] = " + dynamicArray[i]);
-            }
+            dynamicArray.RemoveAt(0);
 
-            Console.WriteLine();
-            int a = dynamicArray[1];
-            Console.WriteLine("dynamicArray[1] = "+a);
+            for (int i = 0; i < dynamicArray.Count; i++)
+            {
+                Console.WriteLine(dynamicArray[i]);
+            }
         }
     }
 }
