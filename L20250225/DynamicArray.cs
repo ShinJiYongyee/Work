1759b97a88e396efd60ffc3d33beea174f9449a5
shinjiyong <shinjiyong0904@gmail.com>
2025-02-25 09:58:54 +0900

    L20250225
    동적 배열 만들기

---
+++ L20250225/DynamicArray.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L20250225
+{
+    public class DynamicArray
+    {
+        int count = 0;
+        int[] ints;
+        public DynamicArray()
+        {
+            ints = new int[0];
+        }
+        public DynamicArray(int count)
+        {
+            ints = new int[count];
+        }
+        public int this[int i]
+        {
+            get { return ints[i]; }
+            set { ints[i] = value; }
+        }
+        public void Add(int data)
+        {
+            count++;
+            int[] newInts = new int[count];
+            for (int i = 0; i < count-1; i++)
+            {
+                newInts[i] = ints[i];
+            }
+            newInts[count-1] = data;
+            ints = newInts;
+        }
+        public void RemoveAt(int index)
+        {
+            int[] newInts = new int[count - 1];
+            for(int i=0; i<index; i++)
+            {
+                newInts[i] = ints[i];
+            }
+            for (int i = index; i < count-1; i++)
+            {
+                newInts[i] = ints[i+1];   
+            }
+            this.ints = newInts;
+            count--;
+        }
+        public int Count()
+        {
+            return count;
+
+        }
+    }
+}
---
+++ L20250225/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace L20250225
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicArray dynamicArray = new DynamicArray();
+            dynamicArray.Add(0);
+            Console.WriteLine($"dynamicArray[{0}] = " + dynamicArray[0]);
+
+            dynamicArray.Add(1);
+            Console.WriteLine($"dynamicArray[{1}] = " + dynamicArray[1]);
+
+            dynamicArray.Add(2);
+            Console.WriteLine($"dynamicArray[{2}] = " + dynamicArray[2]);
+
+            dynamicArray.Add(3);
+            Console.WriteLine($"dynamicArray[{3}] = " + dynamicArray[3]);
+
+            dynamicArray.RemoveAt(2);
+
+            for (int i = 0; i < dynamicArray.Count(); i++)
+            {
+                Console.WriteLine($"dynamicArray[{i}] = " + dynamicArray[i]);
+            }
+            int a = dynamicArray[1];
+            Console.WriteLine(a);
+        }
+    }
+}
