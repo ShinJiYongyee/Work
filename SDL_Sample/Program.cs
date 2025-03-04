e571c4682d43f355de44e953e468d1dd90262246
shinjiyong <shinjiyong0904@gmail.com>
2025-03-04 09:54:09 +0900

    SDL 설치 후 실행
    sayer.sdl2.core 설치

--- SDL_Sample/Program.cs
+++ SDL_Sample/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace SDL_Sample
+﻿using SDL2;
+namespace SDL_Sample
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //HW의 모든 장치를 초기화
+            if( SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING) < 0)  
+            {
+                Console.WriteLine("Fail Init");
+            }
+
+            //IntPtr 자료형으로 정수형 주소 저장
+            IntPtr myWindow =  SDL.SDL_CreateWindow(
+                "Game",
+                100, 100, 
+                640, 480, 
+                SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
+
+            //저장한 자료 제거
+            SDL.SDL_DestroyWindow(myWindow);
+
+            //시작한 SDL 끄기
+            SDL.SDL_Quit();
+
+            //윈도우가 열렸다 사라지면 성공
         }
     }
 }
