caa934e55a00cc3a73df083f7598ef575326a75a
shinjiyong <shinjiyong0904@gmail.com>
2025-03-04 13:59:25 +0900

    원 그리기

--- SDL_Sample/Program.cs
+++ SDL_Sample/Program.cs
@@ -1,9 +1,47 @@
 ﻿using SDL2;
+using System;
 using static SDL2.SDL;
 namespace SDL_Sample
 {
     internal class Program
     {
+        public static void DrawCircle(IntPtr renderer, int centreX, int centreY, int radius)
+        {
+            int diameter = radius * 2;
+
+            int x = (radius - 1);
+            int y = 0;
+            int tx = 1;
+            int ty = 1;
+            int error = (tx - diameter);
+
+            while (x >= y)
+            {
+                // Each of the following renders an octant of the circle
+                SDL_RenderDrawPoint(renderer, centreX + x, centreY - y);
+                SDL_RenderDrawPoint(renderer, centreX + x, centreY + y);
+                SDL_RenderDrawPoint(renderer, centreX - x, centreY - y);
+                SDL_RenderDrawPoint(renderer, centreX - x, centreY + y);
+                SDL_RenderDrawPoint(renderer, centreX + y, centreY - x);
+                SDL_RenderDrawPoint(renderer, centreX + y, centreY + x);
+                SDL_RenderDrawPoint(renderer, centreX - y, centreY - x);
+                SDL_RenderDrawPoint(renderer, centreX - y, centreY + x);
+
+                if (error <= 0)
+                {
+                    ++y;
+                    error += ty;
+                    ty += 2;
+                }
+
+                if (error > 0)
+                {
+                    --x;
+                    tx += 2;
+                    error += (tx - diameter);
+                }
+            }
+        }
         static void Main(string[] args)
         {
 
@@ -48,29 +86,31 @@ namespace SDL_Sample
                 SDL.SDL_SetRenderDrawColor(myRenderer, 255, 255, 255, 0);
                 SDL.SDL_RenderClear(myRenderer);
 
-                //랜덤 사각형 100개 그리기
-                for (int i = 0; i < 100; i++)
-                {
-                    //랜덤 색 정하기
-                    byte r = (byte)(random.Next() % 256);
-                    byte g = (byte)(random.Next() % 256);
-                    byte b = (byte)(random.Next() % 256);
+                //랜덤 색 정하기
+                byte r = (byte)(random.Next() % 256);
+                byte g = (byte)(random.Next() % 256);
+                byte b = (byte)(random.Next() % 256);
 
-                    //랜덤 사각형 만들기
-                    SDL_Rect rect = new SDL_Rect();
-                    rect.x = random.Next(0, 640);
-                    rect.y = random.Next(0, 480);
-                    rect.w = random.Next(0, 640);
-                    rect.h = random.Next(0, 480);
+                //원 좌표와 반지름 정하기
+                int x = random.Next() % 640;
+                int y = random.Next() % 480;
+                int radius = random.Next() % 640;
 
-                    //색 정하기
-                    SDL_SetRenderDrawColor(myRenderer, r, g, b, 0);
-                    //정한 색으로 사각형 칠하기
-                    SDL_RenderFillRect(myRenderer, ref rect);
-                    //사각형 그리기
-                    SDL.SDL_RenderPresent(myRenderer);
+                //원 그리기
+                SDL.SDL_SetRenderDrawColor(myRenderer, r, g, b, 0);
+                Program.DrawCircle(myRenderer, x, y, r);
+                SDL.SDL_RenderPresent(myRenderer);
 
-                }
+                ////원 그리기 2
+                //SDL.SDL_SetRenderDrawColor(myRenderer, r, g, b, 0);
+                //for (int i = 0; i < 360; i++)
+                //{
+                //    int x0 = (int)(radius * Math.Cos(i * (Math.PI / 180.0f)));
+                //    int y0 = (int)(radius * Math.Sin(i * (Math.PI / 180.0f)));
+                //    SDL.SDL_RenderDrawPoint(myRenderer, x + x0, y + y0);
+                //}
+                //SDL.SDL_RenderPresent(myRenderer);
+
 
             }
 
