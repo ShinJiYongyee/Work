e2b7c6167b0b679e85fa3481b656fa9faace28af
shinjiyong <shinjiyong0904@gmail.com>
2025-03-04 09:16:19 +0900

    sdl sample

---
+++ SDL_Sample/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SDL_Sample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
