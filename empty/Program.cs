36cd6ff2c08b403cc8254319556eb83c156761c2
shinjiyong <shinjiyong0904@gmail.com>
2025-03-10 08:53:58 +0900

    empty

---
+++ empty/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace empty
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            int index = 0;
+
+            for (int i = 2; i <= 9; i++)
+            {
+                for(int j = 1; j <= 9; j++)
+                {
+                    list.Add(i*j);
+                }
+            }
+
+            for (int i = 2; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    Console.WriteLine($"{i}x{j}=" + list[index]);
+                    index++;
+                }
+            }
+        }
+    }
+}
