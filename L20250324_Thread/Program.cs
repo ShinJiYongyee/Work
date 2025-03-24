0575fa1c0c0abada22df2465cc5ee4dfb243e3de
shinjiyong <shinjiyong0904@gmail.com>
2025-03-24 15:39:50 +0900

    deadlock

--- L20250324_Thread/Program.cs
+++ L20250324_Thread/Program.cs
@@ -16,27 +16,35 @@ namespace L20250324_Thread
 
         static bool lockTaken = false;
 
-        static Object _lock = new Object(); //작업공간 열쇠
+        static Object _lock1 = new Object(); //작업공간 열쇠
+        static Object _lock2 = new Object(); //작업공간 열쇠
+
         static void Add()
         {
-            lock (_lock)
+            for (int i = 0; i < 100000000; i++)
             {
-                for (int i = 0; i < 100000000; i++)
+                lock (_lock1)
                 {
-                    Money++;
-                    //증감연산 도중 특정 작업공간 내부로 드나들 수 없음
-                    //원자성이 지켜진다
+                    lock (_lock2)
+                    {
+                        Money++;
+                    }
+
                 }
             }
         }
 
         static void Remove()
         {
-            lock (_lock)
+            for (int i = 0; i < 100000000; i++)
             {
-                for (int i = 0; i < 100000000; i++)
+                lock (_lock2)
                 {
-                    Money--;
+                    lock (_lock1)
+                    {
+                        Money--;
+                    }
+
                 }
             }
         }
@@ -51,17 +59,17 @@ namespace L20250324_Thread
             Thread thread2 = new Thread(new ThreadStart(Remove));
 
             //B함수를 따로 실행(Thread)하도록 OS에게 지시
-            thread1.Start();
-            thread2.Start();
 
             //쓰레드의 결과를 화면에 그리지 않음
             //foreground: main thread 종료시 나머지 쓰레드는 다 종료
             thread1.IsBackground = true;
+            thread1.Start();
             thread2.IsBackground = true;
+            thread2.Start();
 
             //thread 종료 시까지 대기
-            //thread1.Join();
-            //thread2.Join();
+            thread1.Join();
+            thread2.Join();
 
             Console.WriteLine($"Money : {Money}");
 
