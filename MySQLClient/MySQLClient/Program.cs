1f86fe0853e4dd5d27e768c1c67a307e8a927d10
shinjiyong <shinjiyong0904@gmail.com>
2025-03-25 14:54:57 +0900

    mysqlclient

---
+++ MySQLClient/MySQLClient/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+using MySQLClient;
+using System.Data;
+using System.Configuration;
+
+namespace MySQLClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //connect
+            string connetctionString = "server=localhost;user=root;database=membership;password=12345678";
+            
+            MySqlConnection mySqlConnection = new MySqlConnection(connetctionString);
+
+            //Task task = mySqlConnector.OpenAsync();  //비동기 처리
+            try
+            {
+                mySqlConnection.Open();
+
+                //login
+                MySqlCommand mySqlCommand1 = new MySqlCommand();
+                mySqlCommand1.Connection = mySqlConnection;
+                mySqlCommand1.CommandText = "select * from membership.users where user_password = 1234 limit 0, 10";
+                mySqlCommand1.Prepare();
+
+                MySqlDataReader dataReader = mySqlCommand1.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    Console.WriteLine(dataReader["name"]+ " " + dataReader["email"]);
+                }
+                dataReader.Close();
+
+                //register
+                MySqlCommand mySqlCommand2 = new MySqlCommand();
+                mySqlCommand2.Connection = mySqlConnection;
+                mySqlCommand2.CommandText = "insert into users (user_id, user_password, name, email) values ( @user_id, @user_password, @name, @email)";
+                mySqlCommand2.Prepare();
+
+                mySqlCommand2.Parameters.AddWithValue("@user_id", "abc001");
+                mySqlCommand2.Parameters.AddWithValue("@user_password", "2222");
+                mySqlCommand2.Parameters.AddWithValue("@name", "신지용");
+                mySqlCommand2.Parameters.AddWithValue("@email", "abc001@abc001.com");
+                mySqlCommand2.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                mySqlConnection.Close();
+            }
+        }
+    }
+}
--- emptyServer/Server/Program.cs
+++ emptyServer/Server/Program.cs
@@ -1,15 +1,61 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Server
 {
+    class Message
+    {
+        public string message;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint listenEndPoint = new IPEndPoint(IPAddress.Any, 4000);
+
+            listenSocket.Bind(listenEndPoint);
+
+            listenSocket.Listen(10);
+
+            Socket clientSocket = listenSocket.Accept();
+
+            byte[] headerBuffer = new byte[2];
+
+            int RecvLength = clientSocket.Receive(headerBuffer, 2, SocketFlags.None);
+     
+            short packetlength = BitConverter.ToInt16(headerBuffer, 0);
+            packetlength =IPAddress.NetworkToHostOrder(packetlength);
+
+            byte[] dataBuffer = new byte[4096];
+            RecvLength = clientSocket.Receive(dataBuffer, packetlength, SocketFlags.None);
+
+            string JsonString = Encoding.UTF8.GetString(dataBuffer, 0, RecvLength);
+
+            Console.WriteLine(JsonString);
+
+            string message = "{\"message\" : \"클라이언트 받고 서버 데이터 추가\"}";
+            byte[] messageBuffer = Encoding.UTF8.GetBytes(message);
+            ushort length =(ushort) IPAddress.HostToNetworkOrder(messageBuffer.Length);
+
+            headerBuffer = BitConverter.GetBytes(length);
+
+            byte[] packetBuffer = new byte[headerBuffer.Length + messageBuffer.Length];
+
+            Buffer.BlockCopy(headerBuffer, 0, packetBuffer, 0, headerBuffer.Length);
+            Buffer.BlockCopy(messageBuffer, 0, packetBuffer, headerBuffer.Length, messageBuffer.Length);
+
+            int sendLength = clientSocket.Send(packetBuffer, packetBuffer.Length, SocketFlags.None);
+
+            clientSocket.Close();
+            listenSocket.Close();
         }
     }
 }
